add8a6981bb1eed752da06d92bbdd5ddcb0c1124
nuri <rlasnfl7288@naver.com>
2023-09-25 18:24:43 +0900

    feat:eslint 수정

--- hooks/useInput.ts
+++
@@ -1,9 +0,0 @@
-import React, { useState, useCallback } from "react";
-
-export default (initValue = null) => {
-  const [value, setter] = useState(initValue);
-  const handler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setter(e.target.value);
-  }, []);
-  return [value, handler];
-};
