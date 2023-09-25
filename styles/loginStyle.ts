f83e79ac70758fd471c50f4560a45a51751d7e28
nuri <rlasnfl7288@naver.com>
2023-09-25 19:50:41 +0900

    feat: 모바일버전 css 수정

--- styles/loginStyle.ts
+++ styles/loginStyle.ts
@@ -4,20 +4,15 @@ import { media } from "./theme";
 export const Container = styled.div`
   background-color: #f5f5f5;
   height: 700px;
-  width: 100%;
+  width: 870px
 
   ${media.medium`
     overflow-x: hidden;
   `}
 `;
 export const ContentWrap = styled.div`
-  width: 820px;
-  margin: 0 auto;
+  width: 100%;
   padding: 85px 100px 0;
-
-  // ${media.medium`
-  //   width: 100%;
-  // `}
 `;
 export const LoginFormBox = styled.div`
   padding: 50px 76px 60px;
