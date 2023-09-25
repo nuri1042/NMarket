2b376e1349b72396afa91f4d80914299182aa0fa
nuri <rlasnfl7288@naver.com>
2023-09-25 19:54:31 +0900

    feat: 모바일버전 css 수정

--- styles/loginStyle.ts
+++ styles/loginStyle.ts
@@ -11,8 +11,8 @@ export const Container = styled.div`
   `}
 `;
 export const ContentWrap = styled.div`
-  width: 100%;
-  padding: 85px 100px 0;
+  // width: 100%;
+  // padding: 85px 100px 0;
 `;
 export const LoginFormBox = styled.div`
   padding: 50px 76px 60px;
